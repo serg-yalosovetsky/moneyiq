783f87c6edb60ae4637aec1a8de97e1c93b00ef1
Serhii Yalosovetskyi <serg.yalosovetsky@gmail.com>
2026-05-31 00:33:52 +0300

    chore: bump versionCode to 3, versionName to 1.0.2

--- moneyiq/app/build.gradle.kts
+++ moneyiq/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "org.pixelrush.moneyiq"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
         multiDexKeepProguard = file("multidex-keep.pro")
     }
 
